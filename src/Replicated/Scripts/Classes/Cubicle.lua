47a975a7ba9d865a896442cb999e62ece0487426
Dustin Haynes <dhaynes@roblox.com>
2022-10-15 17:45:32 -0700

    Refactor, giving more control to the client

--- src/Replicated/Scripts/Classes/Cubicle.lua
+++ src/Replicated/Scripts/Classes/Cubicle.lua
@@ -8,6 +8,7 @@ local Dash = require(ReplicatedStorage.Packages.Dash)
 local Cubicle = Dash.class("Cubicle", function(owner: Player, cframe:CFrame)
     return {
       owner = owner :: Player,
+      humanoid = nil :: Humanoid,
       cframe = cframe or CFrame.new() :: CFrame,
       connection = nil :: RBXScriptConnection,
       model = nil :: Model,
@@ -18,68 +19,41 @@ local Cubicle = Dash.class("Cubicle", function(owner: Player, cframe:CFrame)
     }
 end)
 
-function Cubicle:getOwner()
-  return self.owner
-end
-
-function Cubicle:onLaptopMouseEnter()
-  if self.highlight then
-    self.model.Laptop.Highlight.Enabled = true
-  end
-end
-function Cubicle:onLaptopMouseLeave()
-  if self.highlight then
-    self.model.Laptop.Highlight.Enabled = false
-  end
-end
-function Cubicle:onLaptopMouseClick()
-  self.emitter:Emit(1)
-end
-
 function Cubicle:init()
   self.model = ReplicatedStorage.Assets.Cubicles.Cubicle:Clone()
   self.model:PivotTo(self.cframe)
   self.model.Parent = game.Workspace
 
-  local laptop:Model = self.model.Laptop
-  self.highlight = Instance.new("Highlight")
-  self.highlight.Enabled = false
-  self.highlight.FillTransparency = 1
-  self.highlight.OutlineColor = Color3.fromRGB(24, 193, 255)
-  self.highlight.Parent = laptop
-  
-  --TODO: Make click detector local only
-  local laptopCD = Instance.new("ClickDetector")
-  laptopCD.MouseHoverEnter:Connect(function() 
-    self:onLaptopMouseEnter() 
-  end)
-  laptopCD.MouseHoverLeave:Connect(function() 
-    self:onLaptopMouseLeave() 
-  end)
-  laptopCD.MouseClick:Connect(function() 
-    self:onLaptopMouseClick() 
-  end)
-  laptopCD.Parent = laptop
+  self.humanoid = self.owner.Character.Humanoid
 
---TODO: Make it so that the Seat is the one listening for the player to sit.
-  local humanoid = self.owner.Character.Humanoid
-  humanoid.Seated:Connect(function(bool, seat)
-    if bool then
+  local seat:Seat = self.model.Chair.Seat
+  seat.Changed:Connect(function(propertyName)
+    if propertyName == "Occupant" and seat.Occupant then
       self:startEmitter()
     else
       self:pauseEmitter()
     end
   end)
+
   -- wait a bit to make sure the humanoid can sit properly.
   task.delay(3, function() 
-    self.model.Chair.Seat:Sit(humanoid)
+    self.model.Chair.Seat:Sit(self.humanoid)
   end)
   
   self.emitter = self.model.Laptop.Emitter.ParticleEmitter
   self.emitter.Enabled = false
+  self.emitter.Changed:Connect(function(propertyName)
+    if propertyName == "Enabled" then
+      print("Emitter enabled changed")
+    end
+  end)
 
   local label = self.model.NamePlate.Plate.SurfaceGui.TextLabel
   label.Text = self.owner.Character.Name
+end
+
+function Cubicle:getModel()
+  return self.model
 end
 
 function Cubicle:startEmitter()
--- src/Server/init.server.lua
+++ src/Server/init.server.lua
@@ -6,29 +6,34 @@ local CollectionService = game:GetService("CollectionService")
 local Classes = ReplicatedStorage.Scripts.Classes
 local Cubicle = require(Classes.Cubicle)
 
-local cubicles = {}
-local function createCubicle(player:Player, playerIndex)
-    if cubicles[player] then return end
-    local cframe = CFrame.new((playerIndex - 1) * 10, 0, 0)
-    local cubicle = Cubicle.new(player, cframe):init()
-    CollectionService:AddTag(cubicle, "Cubicle")
-    cubicles[player] = cubicle
-end
+local OnCubicleAdded:RemoteEvent = ReplicatedStorage.Scripts.Events.OnCubicleAdded
 
+local cubicles = {}
 local playerIndex = 0
 local function onPlayerAdded(player:Player)
     player.CharacterAdded:Connect(function(character)
+        if cubicles[player] then 
+            return
+        end
+        
         playerIndex = playerIndex + 1
-        createCubicle(player, playerIndex)
+        
+        local cframe = CFrame.new((playerIndex - 1) * 10, 0, 0)
+        local cubicle = Cubicle.new(player, cframe)
+        cubicle:init()
+
+        local model = cubicle.model
+        
+        CollectionService:AddTag(model, "Cubicle")
+        cubicles[player] = cubicle
+
+        OnCubicleAdded:FireClient(player, model)
+        print("Server: Cubicle added and assigned to", player)
     end)
 end
 
--- Players.PlayerRemoving:Connect(function(player)
---     --remove the cubicle
---     --reorganize the cubicles
--- end)
-
 Players.PlayerAdded:Connect(onPlayerAdded)
+
 --Loop through all connected players to make sure each player is added!
 for _, player in pairs(Players:GetPlayers()) do
     onPlayerAdded(player)
